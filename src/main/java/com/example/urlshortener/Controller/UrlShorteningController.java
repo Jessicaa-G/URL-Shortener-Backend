208a7c4273194c4a3b3927d939e0711a85a5da87
Sonya <sonya@SonyadeMacBook-Pro.local>
2023-10-19 21:23:41 -0500

    add bigtable connection

--- src/main/java/com/example/urlshortener/Controller/UrlShorteningController.java
+++ src/main/java/com/example/urlshortener/Controller/UrlShorteningController.java
@@ -8,9 +8,7 @@ import com.example.urlshortener.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UrlShorteningController {
@@ -32,5 +30,11 @@ public class UrlShorteningController {
         UrlErrorResponse error = new UrlErrorResponse("404", "Error occurred when shortening url");
         return new ResponseEntity<UrlErrorResponse>(error, HttpStatus.OK);
 
+    }
+
+    @DeleteMapping("shortenurl")
+    public ResponseEntity<?> deleteUrl(@RequestParam String url) {
+        urlService.deleteUrlById(url);
+        return new ResponseEntity<UrlResponse>(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/urlshortener/service/UrlService.java
+++ src/main/java/com/example/urlshortener/service/UrlService.java
@@ -2,15 +2,39 @@ package com.example.urlshortener.service;
 
 import com.example.urlshortener.model.Url;
 import com.example.urlshortener.model.UrlDto;
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
+import com.google.cloud.bigtable.data.v2.models.Row;
+import com.google.cloud.bigtable.data.v2.models.RowCell;
+import com.google.cloud.bigtable.data.v2.models.RowMutation;
 import com.google.common.hash.Hashing;
 import io.micrometer.common.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class UrlService {
+
+    private BigtableDataClient dataClient;
+
+    @Value("${URL_TABLE_ID}")
+    private String tableId;
+
+    private final String COLUMN_FAMILY_URL="url_details";
+
+    private final String COLUMN_FAMILY_TIME="time_details";
+
+    private final String COLUMN_FAMILY_CLICK="click_details";
+
+    public UrlService(@Value("${PROJECT_ID}") final String projectId, @Value("${INSTANCE_ID}") final String instanceId) throws IOException {
+        BigtableDataSettings settings = BigtableDataSettings.newBuilder().setProjectId(projectId).setInstanceId(instanceId).build();
+        this.dataClient = BigtableDataClient.create(settings);
+    }
 
     public Url generateShortUrl(UrlDto url) {
         if (StringUtils.isNotEmpty(url.getUrl())) {
@@ -20,9 +44,39 @@ public class UrlService {
             shortenedUrl.setLongUrl(url.getUrl());
             shortenedUrl.setShortUrl(shortenedStr);
             shortenedUrl.setExpireDate(getExpireDate(url.getExpireDate(), shortenedUrl.getCreateDate()));
+
+            String rowkey = shortenedUrl.getShortUrl();
+            RowMutation rowMutation = RowMutation.create(tableId, rowkey)
+                    .deleteCells(COLUMN_FAMILY_URL, "longUrl")
+                    .setCell(COLUMN_FAMILY_URL, "longUrl", shortenedUrl.getLongUrl())
+//                      TODO: short url is already in rowkey
+//                    .deleteCells(COLUMN_FAMILY_URL, "shortUrl")
+//                    .setCell(COLUMN_FAMILY_URL, "shortUrl", shortenedUrl.getShortUrl())
+                    .deleteCells(COLUMN_FAMILY_TIME, "createDate")
+                    .setCell(COLUMN_FAMILY_TIME, "createDate", shortenedUrl.getCreateDate().toEpochSecond(ZoneOffset.UTC))
+                    .deleteCells(COLUMN_FAMILY_TIME, "expireDate")
+                    .setCell(COLUMN_FAMILY_TIME, "expireDate", shortenedUrl.getExpireDate().toEpochSecond(ZoneOffset.UTC));
+
+            dataClient.mutateRow(rowMutation);
+            getLongUrlById(shortenedUrl.getShortUrl());
             return shortenedUrl;
         }
         return null;
+    }
+
+    private void getLongUrlById(String shortUrl) {
+        Row row = dataClient.readRow(tableId, shortUrl);
+        System.out.println("Row: " + row.getKey().toStringUtf8());
+        for (RowCell cell : row.getCells()) {
+            System.out.printf(
+                    "Family: %s    Qualifier: %s    Value: %s%n",
+                    cell.getFamily(), cell.getQualifier().toStringUtf8(), cell.getValue().toStringUtf8());
+        }
+    }
+
+    public void deleteUrlById(String shortUrl) {
+        RowMutation rowMutation = RowMutation.create(tableId, shortUrl).deleteRow();
+        dataClient.mutateRow(rowMutation);
     }
 
     private String shortenUrl(String url) {
