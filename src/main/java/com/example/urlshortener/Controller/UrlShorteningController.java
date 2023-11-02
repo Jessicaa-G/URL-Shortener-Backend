b0b520e55394a16e2ee03c5596cfc0414d625217
jessica <7987556+jessicaguan@user.norelpy.gitee.com>
2023-11-02 15:07:40 -0500

    Revise redirect endpoint

--- src/main/java/com/example/urlshortener/Controller/UrlShorteningController.java
+++ src/main/java/com/example/urlshortener/Controller/UrlShorteningController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +18,7 @@ import java.util.Map;
 public class UrlShorteningController {
     @Autowired
     private UrlService urlService;
+    private final String BASE_ADDR = "http://localhost:8080";
 
     @PostMapping("shortenurl")
     public ResponseEntity<?> generateShortUrl(@RequestBody UrlDto urlDto) {
@@ -26,7 +28,7 @@ public class UrlShorteningController {
             UrlResponse res = new UrlResponse();
             res.setLongUrl(shortenedUrl.getLongUrl());
             res.setExpireDate(shortenedUrl.getExpireDate());
-            res.setShortUrl(shortenedUrl.getShortUrl());
+            res.setShortUrl(BASE_ADDR + "/shortened/" + shortenedUrl.getShortUrl());
             return new ResponseEntity<UrlResponse>(res, HttpStatus.OK);
         }
 
@@ -41,13 +43,14 @@ public class UrlShorteningController {
         return new ResponseEntity<UrlResponse>(HttpStatus.OK);
     }
 
-    @GetMapping("redirect/{urlParam}")
-    public ResponseEntity<?> redirect(@PathVariable String urlParam) {
+    @GetMapping("shortened/{urlParam}")
+    public Object redirect(@PathVariable String urlParam) {
         String longUrl = urlService.redirect(urlParam);
         if (longUrl != null) {
-            Map<String, String> response = new HashMap<>();
-            response.put("longUrl", longUrl);
-            return ResponseEntity.ok(response);
+            // Redirect to long url
+            RedirectView redirectView = new RedirectView();
+            redirectView.setUrl(longUrl);
+            return redirectView;
         }
 
         UrlErrorResponse error = new UrlErrorResponse("404", "Error occurred when shortening url");
